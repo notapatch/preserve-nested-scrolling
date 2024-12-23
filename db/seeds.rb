684766288a875e05ee1b6592d937f70b13a69343
Julian Wigley <julian.wigley@i2fast.com>
2024-12-23 14:57:47 +0000

    Demo nested delete and seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,7 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+1.upto(30) do |i|
+  Post.find_or_create_by(title: i.ordinalize)
+end
